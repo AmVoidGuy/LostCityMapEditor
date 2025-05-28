c5993bc1d0a5c0093b196c802548180a5eb014d0
AmVoidGuy <132288247+AmVoidGuy@users.noreply.github.com>
2025-05-27 23:04:24 -0400

    Dependency and module cleanup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,21 +1,13 @@
 module org.lostcitymapeditor {
     requires javafx.controls;
-    requires javafx.fxml;
 
-    requires org.controlsfx.controls;
     requires java.desktop;
     requires javafx.swing;
     requires org.joml;
     requires org.lwjgl.glfw;
     requires org.lwjgl.opengl;
-    requires org.lwjgl.nanovg;
-    requires imgui.binding;
-    requires imgui.lwjgl3;
-    requires org.reflections;
 
-    opens org.lostcitymapeditor to javafx.fxml;
     exports org.lostcitymapeditor.Renderer to javafx.graphics;
 
-    opens org.lostcitymapeditor.Renderer to javafx.fxml;
     exports org.lostcitymapeditor;
 }
